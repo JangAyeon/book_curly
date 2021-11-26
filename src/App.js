56ae7c0a843d9cd736c4a6650aab2ccc5a299095
JangAyeon <ayeon3052@ewhain.net>
2021-11-26 13:11:59 +0900

    #2 login page css

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.scss';
+import React,{Component} from "react";
+import Login from "./Pages/Login/Login";
+
+function App() {
+  return (
+    <div className="App">
+      <h5>test</h5>
+      <Login></Login>
+
+    </div>
+  );
+}
+
+export default App;
