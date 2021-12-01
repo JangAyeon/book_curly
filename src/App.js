bece18c977f9e042a610d1ad611077ab16b24feb
JangAyeon <ayeon3052@ewhain.net>
2021-12-01 12:56:18 +0900

    #4 Footer mark up

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import './App.scss';
 import React,{Component} from "react";
 import Login from "./Pages/Login/Login";
 
+import Footer from "./Components/Footer/Footer";
+
 function App() {
   return (
     <div className="App">
       <h5>test</h5>
-      <Login></Login>
+      {/*<Login></Login>*/}
+      <Footer></Footer>
+
+
 
     </div>
   );
