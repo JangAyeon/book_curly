239c0500fce8a51d812cc88be45a128a2f0d68bb
JangAyeon <ayeon3052@ewhain.net>
2022-01-15 22:01:51 +0900

    signup title

--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -6,11 +6,42 @@ class Signup extends Component {
   render() {
     return (
       <div className="signup_main">
-        <div className="signup_content">
+        <div className="signup_title">
+          <h2>회원가입</h2>
           <p className="signup_sub">
             <span className="ico">*</span>
             필수입력사항
           </p>
+        </div>
+        <div className="signup_content">
+          <table className="tbl_signup">
+            <tbody>
+              <tr className="signup_id">
+                <th>
+                  아이디
+                  <span className="ico">
+                    {" "}
+                    *<span className="screen_out">필수항목</span>
+                  </span>
+                </th>
+              </tr>
+              <tr></tr>
+              <tr></tr>
+              <tr></tr>
+
+              <tr></tr>
+              <tr></tr>
+              <tr></tr>
+              <tr></tr>
+
+              <tr></tr>
+              <tr></tr>
+              <tr></tr>
+              <tr></tr>
+
+              <tr></tr>
+            </tbody>
+          </table>
         </div>
       </div>
     );
