591da9c03d6d99094c10ecd5e932317b63c22e95
JangAyeon <ayeon3052@ewhain.net>
2022-01-30 23:12:33 +0900

    #3

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import "./App.css";
 import Footer from "./Components/Footer/Footer";
 
-import Loign from "./Pages/Login/Login";
-import Signup from "./Pages/Signup/Signup";
-
 function App() {
   return (
     <>
-      <Signup />
-      <Loign />
       <Footer />
     </>
   );
