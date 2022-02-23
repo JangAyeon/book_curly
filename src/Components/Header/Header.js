a48dae40114c0c8c77f943b2ff2800d0b1a07189
JangAyeon <ayeon3052@ewhain.net>
2022-02-24 01:40:56 +0900

    #15 2step header & 1step hover css

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -75,7 +75,7 @@ const Navbar = () => {
           <li>
             전체 카테고리
             <ul className="Navbar_sub">
-              <li className="sub">
+              <li>
                 국내 도서
                 <ul className="sub_sub">
                   <li>소설</li>
@@ -89,9 +89,38 @@ const Navbar = () => {
                   <li>유라</li>
                 </ul>
               </li>
-              <li className="sub">외국 도서</li>
-              <li className="sub">음반</li>
-              <li className="sub">DVD</li>
+              <li className="sub">
+                외국 도서
+                <ul className="sub_sub">
+                  <li>소설</li>
+                  <li>시/에세이</li>
+                  <li>에술/대중문화</li>
+                  <li>사회과학</li>
+                  <li>역사와 문화</li>
+                </ul>
+              </li>
+              <li className="sub">
+                음반
+                <ul className="sub_sub">
+                  <li>소설</li>
+                  <li>시/에세이</li>
+                  <li>에술/대중문화</li>
+                </ul>
+              </li>
+              <li className="sub">
+                DVD{" "}
+                <ul className="sub_sub">
+                  <li>소설</li>
+                  <li>시/에세이</li>
+                  <li>에술/대중문화</li>
+                  <li>사회과학</li>
+                  <li>역사와 문화</li>
+                  <li>사회과학</li>
+                  <li>잡지</li>
+                  <li>만화</li>
+                  <li>유라</li>
+                </ul>
+              </li>
             </ul>
           </li>
         </span>
